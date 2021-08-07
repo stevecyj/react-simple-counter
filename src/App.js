c6dfaa7e9ca794d1785d2e9f54a670829eba13a8
root <root@localhost.localdomain>
2021-08-07 09:02:13 +0800

    Feat:counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+import { useState } from "react";
+
+const defaultNumber = 20;
 function App() {
+  let [counter, setCounter] = useState(defaultNumber);
+
+  const hidden = {
+    visibility: counter >= 10 && "hidden",
+  };
+
+  const handleClick = (type) => (e) => {
+    console.log(e.target);
+    if (type === "increase") {
+      setCounter((prevCounter) => prevCounter + 1);
+    } else if (type === "decrease") {
+      setCounter((prevCounter) => prevCounter - 1);
+    }
+  };
+  // const handleIncrease = () => {
+  //   console.log("handleIncrease", counter);
+  //   setCounter(counter + 1);
+  // };
+
+  // const handleDecrease = () => {
+  //   console.log("handleDecrease", counter);
+  //   setCounter(counter - 1);
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div
+        className="chevron chevron-up"
+        onClick={handleClick("increase")}
+        style={hidden}
+      ></div>
+      <div className="number">{counter}</div>
+      <div
+        className="chevron chevron-down"
+        onClick={handleClick("decrease")}
+      ></div>
     </div>
   );
 }
